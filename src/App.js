e893dcae59be09b2cbd2dc00e452e842848a27b5
Justin Green <justingreenmedia@gmail.com>
2021-07-14 14:59:46 -0500

    Project files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Accordion from './components/Accordion';
+import { accordionData } from './utilities/data';
 
-function App() {
+function App() {  
+  const {id, title, content} = accordionData;
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Accordion</h1>
+      <div className="accordion">
+        {accordionData.map(({title, content, id}) => (
+          <Accordion title={title} content={content} key={id} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/utilities/data.js
@@ -0,0 +1,22 @@
+export const accordionData = [
+	{
+		id: 1,
+		title: 'Section 1',
+		content: 'This is accordion data for section one'
+	},
+	{
+		id: 2,
+		title: 'Section 2',
+		content: 'This is accordion data for section two'
+	},
+	{
+		id: 3,
+		title: 'Section 3',
+		content: 'This is accordion data for section three'
+	},
+	{
+		id: 4,
+		title: 'Section 4',
+		content: 'This is accordion data for section four'
+	}
+  ]
